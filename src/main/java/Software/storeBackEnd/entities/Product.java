b7f6c38ca8b23d7a70b134727824f9514c6e05a5
AhmedAbdallah142 <ahmed.abdalla_142@yahoo.com>
2021-12-14 18:48:26 +0200

    controller end

--- src/main/java/Software/storeBackEnd/entities/Product.java
+++ src/main/java/Software/storeBackEnd/entities/Product.java
@@ -2,5 +2,29 @@ package Software.storeBackEnd.entities;
 
 public class Product {
     double price;
-    String name;
+    String id,name,category;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
