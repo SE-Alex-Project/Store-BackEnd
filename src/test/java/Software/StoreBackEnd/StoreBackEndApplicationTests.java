68c928dcc5e8f98e36a43e6d8bab9f79f32132f7
AhmedAbdallah142 <ahmed.abdalla_142@yahoo.com>
2021-12-14 14:59:42 +0200

    upload project

---
+++ src/test/java/Software/StoreBackEnd/StoreBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package Software.StoreBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
