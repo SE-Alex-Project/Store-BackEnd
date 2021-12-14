28cc8c0b99056842d61faefc00e8fdeff723c0ca
Ahmed4Elfax <ahmed55fax@gmail.com>
2021-12-14 20:52:07 +0200

    product

--- src/main/java/Software/storeBackEnd/entities/Product.java
+++ src/main/java/Software/storeBackEnd/entities/Product.java
@@ -1,6 +1,25 @@
 package Software.storeBackEnd.entities;
 
+import java.util.ArrayList;
+
 public class Product {
     double price;
     String name;
+    ArrayList<Byte[]> images = new ArrayList<Byte[]>();
+    
+    public void addImage(Byte[] photo) {
+    	images.add(photo);
+    }
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
