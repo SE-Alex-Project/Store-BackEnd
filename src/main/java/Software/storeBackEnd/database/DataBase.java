610ee0bc43d825e6ee3fd81a4bbb48b56eff5eda
AhmedAbdallah142 <ahmed.abdalla_142@yahoo.com>
2021-12-23 00:52:03 +0200

    connect to database

--- src/main/java/Software/storeBackEnd/database/DataBase.java
+++ src/main/java/Software/storeBackEnd/database/DataBase.java
@@ -2,6 +2,7 @@ package Software.storeBackEnd.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class DataBase {
@@ -10,11 +11,19 @@ public class DataBase {
     private static DataBase instance = null;
     private DataBase(){
         try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//            Connection con= DriverManager.getConnection(
+//                    "jdbc:mysql://127.0.0.1:3306/storeDB","Store","12345");
             Class.forName("com.mysql.jdbc.Driver");
             Connection con= DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/sonoo","root","root");
-            //here sonoo is database name, root is username and password
+                    "jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11460629","sql11460629","5vZvcuYbs9");
             stmt = con.createStatement();
+//            ResultSet st = stmt.executeQuery("SELECT * FROM Product");
+////            stmt.execute("INSERT INTO Product VALUES (12,'WATCh', 4.02 , 'ahmed watch')");
+//            //System.out.println(st.getInt(0));
+//            System.out.println(st.next());
+//            System.out.println(st.getString(4));
+
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
