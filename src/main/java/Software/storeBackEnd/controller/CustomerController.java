299223661864266ef4aa65d1f1ac6b75f8720b27
Ahmed4Elfax <ahmed55fax@gmail.com>
2021-12-28 23:50:24 +0200

    addToCart

--- src/main/java/Software/storeBackEnd/controller/CustomerController.java
+++ src/main/java/Software/storeBackEnd/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package Software.storeBackEnd.controller;
 
-import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+
 import org.springframework.web.bind.annotation.*;
 
 import Software.storeBackEnd.authentication.Authentication;
@@ -15,6 +16,9 @@ public class CustomerController extends Authentication{
 
 	private final CustomerDatabase customerDataBase = new CustomerDatabase();
 
+	/*
+	 * token
+	 */
 	@PostMapping("/buy")
     public String buyCart(@RequestBody String token) throws Exception {
       String email = getUser(token);
@@ -23,9 +27,18 @@ public class CustomerController extends Authentication{
       return customerDataBase.buyCart(cart);
     }
 	
-	
+	/*add To Cart json format
+    {
+    "token":token,
+    "product": product_id
+    }
+     */
     @PostMapping("/addToCart")
-    public void addToCart(@RequestBody JSONArray products_id){
+    public void addToCart(@RequestBody JSONObject addToCartJson) throws Exception{
+    	String email = getUser(addToCartJson.getAsString("token"));
+        String cartId = customerDataBase.getCart(email);
+        String product_id = addToCartJson.getAsString("product_id");
+        customerDataBase.addToCart(Integer.parseInt(product_id), Integer.parseInt(cartId));
     }
 
   
