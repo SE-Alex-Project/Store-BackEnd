9c9eb376ca402cca4ddc9fabf5cc585a8021b0de
AhmedAbdallah142 <ahmed.abdalla_142@yahoo.com>
2022-01-02 00:16:43 +0200

    online database

--- src/main/java/Software/storeBackEnd/database/Database.java
+++ src/main/java/Software/storeBackEnd/database/Database.java
@@ -22,8 +22,8 @@ public class Database {
 //            Connection con= DriverManager.getConnection(
 //                    "jdbc:mysql://127.0.0.1:3306/storeDB","Store","12345");
             Class.forName("com.mysql.jdbc.Driver");
-            //  con = DriverManager.getConnection("jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11460629", "sql11460629", "5vZvcuYbs9");
-            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/storeDB", "Store", "12345");
+            con = DriverManager.getConnection("jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11462667", "sql11462667", "UccaRWmjdb");
+//            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/storeDB", "Store", "12345");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
