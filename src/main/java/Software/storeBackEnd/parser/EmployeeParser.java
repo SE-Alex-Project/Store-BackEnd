f4a6c080e915b0243360e6ea03129bbb3d9e13ce
Ahmed4Elfax <ahmed55fax@gmail.com>
2022-02-08 14:30:07 +0200

    parser

---
+++ src/main/java/Software/storeBackEnd/parser/EmployeeParser.java
@@ -0,0 +1,19 @@
+package Software.storeBackEnd.parser;
+
+import static net.minidev.json.parser.JSONParser.DEFAULT_PERMISSIVE_MODE;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
+
+public class EmployeeParser {
+
+	public static JSONObject parseEmployee(ResultSet resultSet) throws SQLException, ParseException {
+        return (JSONObject) new JSONParser(DEFAULT_PERMISSIVE_MODE).parse("{\"email\":'" + resultSet.getString("email") + "',\"fName\":'"
+                + resultSet.getString("fName") + "',\"lName\":'" + resultSet.getString("lName") + "',\"storeId\":'" +
+                resultSet.getString("storeId") + "'}");
+    }
+}
